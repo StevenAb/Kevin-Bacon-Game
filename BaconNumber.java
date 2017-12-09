a693b131d03f3a8c381ac7c896900d2dc693aab0
StevenAb <shadwclaw@gmail.com>
2017-12-08 23:56:58 -0500

    Fixed find with breadth first search and added extra features

--- BaconNumber.java
+++ BaconNumber.java
@@ -4,6 +4,7 @@ import java.net.URL;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.Scanner;
 import java.util.Set;
 import java.util.Stack;
@@ -13,13 +14,16 @@ public class BaconNumber {
 	private String center;
 	private MyGraph graph;
 	private Set<String> visited;
-	private int distance;
-	private HashMap<String,String> lookup;
+	private int distance, reachable, unreachable;
 	private Stack<String> path;
+	private HashMap<String, Stack<String>> lookup;
+	private HashMap<String, Integer> distLookup;
+	private LinkedList<Vertex> queue;
 	
 	public BaconNumber(String file) {
 		graph = new MyGraph();
-		lookup = new HashMap<String,String>();
+		lookup = new HashMap<String,Stack<String>>();
+		distLookup = new HashMap<String, Integer>();
 		
 		Scanner scanny = null;
 		try {
@@ -29,71 +33,305 @@ public class BaconNumber {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+		int count = 0;
 		
 		String[] s = null;
 		while (scanny.hasNextLine()) {
 			s = scanny.nextLine().split("\\|");
-			graph.add(s[0]);
-			graph.add(s[1]);
-			graph.connect(s[0], s[1]);
+			graph.add(s[0].toLowerCase());
+			graph.add(s[1].toLowerCase());
+			graph.connect(s[0].toLowerCase(), s[1].toLowerCase());
+			count++;
+			if (count >= 300000 && count % 300000 == 0) {
+				System.out.println(count + " lines processed...");
+			}
 		}
 		
-		System.out.println(graph.size());
+		recenter("Kevin Bacon (I)".toLowerCase());
 	}
 	
 	private boolean recenter(String v) {
-		if (graph.contains(v)) {
-			center = v;
+		if (graph.contains(v.toLowerCase())) {
+			center = v.toLowerCase();
 			return true;
 		} else {
 			return false;
 		}
 	}
-	
 	/**
-	 * thisDistance
-	 * queue.addall(getConnections)
-	 * dist(v, distance+1)
-	 */
-	
-	private boolean dist(String v, int thisDist) {
-		if (v.equals(center)) {
-			this.distance = thisDist;
-			path.push(v);
-			return true;
-		}
-		
-		for (String node : graph.getConnections(v)) {
-			if (!visited.contains(node)) {
-				visited.add(node);
-				if (dist(v, thisDist + 1)) {
+	private boolean dist(String v, int thisDist) {			
+		while (!queue.isEmpty()) {
+			//String s = queue.remove(queue.size()-1);
+			visited.add(s);
+			if (v.equals(center)) {
+				this.distance = thisDist;
+				path.clear();
+				path.push(v);
+				return true;
+			}
+			if (!visited.contains(s)) {
+				if (dist(s, thisDist + 1)) {
 					path.push(v);
+					ArrayList<Object> arr = new ArrayList<Object>();
+					arr.add(this.distance - thisDist);
+					arr.addAll(path);
+					lookup.put(v, arr);
 					return true;
 				}
 			}
 		}
-		
+			
 		return false;
+	}
+	*/
+	
+	//Vertex class inspired by the book
+	class Vertex {
+		String name;
+		Vertex parent;
+		int distance;
+		
+		public Vertex(String n, Vertex p, int d) {
+			this.name = n;
+			this.parent = p;
+			this.distance = d;
+		}
 	}
 	
 	public void find(String v) {
 		if (!graph.contains(v)) {
+			System.out.println(v + " is not in this data set");
 			return;
 		}
 		
-		distance = Integer.MAX_VALUE;
+		if (lookup.containsKey(v)) {
+			Stack<String> pa = (Stack<String>) lookup.get(v).clone();
+			System.out.print("Distance: " + Integer.parseInt(pa.pop())/2 + ", Path: ");
+			System.out.print(pa.pop());
+			while (!pa.isEmpty()) {
+				System.out.print(" --> " + pa.pop());
+			}
+			System.out.println();
+			return;
+		}
+		
 		visited = new HashSet<String>();
 		path = new Stack<String>();
+		queue = new LinkedList<Vertex>();
+		Vertex start = new Vertex(v, null, 0);
+		queue.add(start);
 		
-		if (dist(v, 0)) {
-			System.out.print("Distance: " + distance + ", Path: ");
-			for (String string : path) {
-				System.out.println(path.pop() + " --> ");
+		while(!queue.isEmpty()) {
+			Vertex vert = queue.remove();
+			visited.add(vert.name);
+			
+			if (vert.name.equals(center)) {
+				solution(vert);
+				return;
+			} else {
+				for (String str : graph.getConnections(vert.name)) {
+					if (!visited.contains(str)) {
+						Vertex ve = new Vertex(str, vert, vert.distance + 1);
+						queue.add(ve);
+					}
+				}
+			}
+		}
+		
+		System.out.println(v + " is unreachable");
+	}
+	
+	private void solution(Vertex v) {
+		distance = v.distance;
+		Vertex current = v;
+		while (current != null) {
+			path.push(current.name);
+			if (!lookup.containsKey(current.name)) {
+				@SuppressWarnings("unchecked")
+				Stack<String> st = (Stack<String>) path.clone();
+				st.push(Integer.toString(distance - current.distance));
+				lookup.put(current.name, st);
+			}
+			current = current.parent;
+		}
+		System.out.print("Distance: " + distance/2 + ", Path: ");
+		System.out.print(path.pop());
+		while (!path.isEmpty()) {
+			System.out.print(" --> " + path.pop());
+		}
+		System.out.println();
+	}
+	
+	public void table() {
+		int count = 0;
+		reachable = 0;
+		unreachable = 0;
+		HashMap<Integer,Integer> mappy = new HashMap<Integer,Integer>();
+		
+		for (String v : graph.getVerticies()) {
+			int current = -2;
+			if (v.charAt(v.indexOf("(") + 1) != '1' && v.charAt(v.indexOf("(") + 1) != '2') {
+				current = findDist(v);
+			}
+			if (current != -2) {
+				if (!mappy.containsKey(current)) {
+					mappy.put(current, 1);
+				} else {
+					mappy.put(current, mappy.get(current) + 1);
+				}
+			}
+			count++;
+			if (count >= 750 && count % 750 == 0) {
+				System.out.println(Math.round(((float) count/graph.size())*100) + "% complete...");
+			}
+		}
+		
+		System.out.println();
+		System.out.println("Table of distances for " + center);
+		for (Integer in : mappy.keySet()) {
+			if (in == -1) {
+				System.out.println("Unreachable: " + mappy.get(in));
+			} else {
+				System.out.println("Number " + in + ": " + mappy.get(in));
 			}
 		}
 	}
 	
-	public void main(String[] args) {
+	public void avgdist(String center) {
+		recenter(center);
+		int avg = 0;
+		int count = 0;
+		reachable = 0;
+		unreachable = 0;
+		
+		for (String v : graph.getVerticies()) {
+			int current = -1;
+			if (v.charAt(v.indexOf("(") + 1) != '1' && v.charAt(v.indexOf("(") + 1) != '2') {
+				current = findDist(v);
+			}
+			if (current != -1) {
+				avg += current;
+				count++;
+			}
+			if (count >= 750 && count % 750 == 0) {
+				System.out.println(Math.round(((float) count/graph.size())*100) + "% complete...");
+			}
+		}
+		
+		System.out.println("" + ((float) avg/count) + " " + center + " (" + reachable + ", " + unreachable + ")");
+	}
+	
+	public void avgdist() {
+		int avg = 0;
+		int count = 0;
+		reachable = 0;
+		unreachable = 0;
+		
+		for (String v : graph.getVerticies()) {
+			int current = -1;
+			if (v.charAt(v.indexOf("(") + 1) != '1' && v.charAt(v.indexOf("(") + 1) != '2') {
+				current = findDist(v);
+			}
+			if (current != -1) {
+				avg += current;
+				count++;
+			}
+			if (count >= 750 && count % 750 == 0) {
+				System.out.println(Math.round(((float) count/graph.size())*100) + "% complete...");
+			}
+		}
+		
+		System.out.println("" + ((float) avg/count) + " " + center + " (" + reachable + ", " + unreachable + ")");
+	}
+	
+	public int findDist(String v) {
+		if (!graph.contains(v)) {
+			return -1;
+		}
+		
+		if (distLookup.containsKey(v)) {
+			reachable++;
+			return distLookup.get(v)/2;
+		} else if (lookup.containsKey(v)) {
+			Stack<String> pa = (Stack<String>) lookup.get(v).clone();
+			reachable++;
+			return Integer.parseInt(pa.pop())/2;
+		}
+		
+		visited = new HashSet<String>();
+		path = new Stack<String>();
+		queue = new LinkedList<Vertex>();
+		Vertex start = new Vertex(v, null, 0);
+		queue.add(start);
+		
+		while(!queue.isEmpty()) {
+			Vertex vert = queue.remove();
+			visited.add(vert.name);
+			
+			if (vert.name.equals(center)) {
+				reachable++;
+				distSol(vert);
+				return vert.distance/2;
+			} else {
+				for (String str : graph.getConnections(vert.name)) {
+					if (!visited.contains(str)) {
+						Vertex ve = new Vertex(str, vert, vert.distance + 1);
+						queue.add(ve);
+					}
+				}
+			}
+		}
+		
+		unreachable++;
+		return -1;
+	}
+	
+	private void distSol(Vertex v) {
+		Vertex current = v;
+		int oDist = current.distance;
+		while (current != null) {
+			if (!distLookup.containsKey(current.name)) {
+				distLookup.put(current.name, oDist - current.distance);
+			}
+			current = current.parent;
+		}
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("Loading...");
 		BaconNumber bacon = new BaconNumber(args[0]);
+		Scanner scanny = new Scanner(System.in);
+		System.out.println();
+		System.out.println("Welcome to the Kevin Bacon Game!");
+		System.out.println("Use \"find [name]\" to find the Bacon number of [name]");
+		System.out.println("Use \"recenter [name]\" to center the game at [name]");
+		System.out.println();
+		while(scanny.hasNextLine()) {
+			String s = scanny.nextLine().toLowerCase();
+			if (s.contains("find")) {
+				if (s.length() <= 5) {
+					break;
+				}
+				System.out.println("Searching for " + s.substring(5).toLowerCase());
+				bacon.find(s.substring(5).toLowerCase());
+			} else if (s.contains("recenter")) {
+				if (s.length() <= 8) {
+					break;
+				}
+				bacon.recenter(s.substring(9).toLowerCase());
+				System.out.println("New center: " + s.substring(9).toLowerCase());
+				System.out.println();
+			} else if (s.contains("avgdist")) {
+				System.out.println("Finding average distance...");
+				bacon.avgdist();
+			} else if (s.contains("table")) {
+				System.out.println("Finding distance table...");
+				bacon.table();
+			} else {
+				System.out.println("\"" + s + "\" is an invalid command");
+			}
+			
+			System.out.println();
+		}
 	}
 }
